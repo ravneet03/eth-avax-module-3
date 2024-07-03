282d3cb8e0150b1ae44bb3b667319a6795b70a88
Ravneet Singh <ravneetsingh290@gmail.com>
2024-07-03 23:13:55 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,29 +1,32 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.20;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.0/contracts/token/ERC20/ERC20.sol";
 
-contract token is ERC20 {
+contract Token is ERC20 {
 
     address private owner;
+
     constructor() ERC20("SLOY", "SLY") {
         owner = msg.sender;
-       _mint(owner, 100 * 10**uint(decimals()));
+        _mint(owner, 100 * 10 ** uint(decimals()));
     }
 
-    modifier auntheticate(){
-        require(owner == msg.sender );
+    modifier authenticate() {
+        require(owner == msg.sender, "Not authorized");
         _;
-     }
-
-    function mint (address receiver,uint256 amount) public payable auntheticate {
-        _mint(receiver,amount);
     }
 
-    function burn(address receiver,uint256 amount) public {
-        _burn(receiver,amount);
+    function mint(address receiver, uint256 amount) public authenticate {
+        _mint(receiver, amount);
     }
 
+    function burn(address receiver, uint256 amount) public {
+        _burn(receiver, amount);
+    }
 
-
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
 }
